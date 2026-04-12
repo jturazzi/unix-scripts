7c87db41dd1b0bb7f5a3e16910e68a4a684ce73a
Jérémy Turazzi <jeremy@trz.fr>
2026-04-12 12:30:07 +0200

    Correction du fichier motd

--- debian/motd/remove.sh
+++ debian/motd/remove.sh
@@ -12,9 +12,9 @@ fi
 : > /etc/motd
 
 # Supprime tous les fichiers de génération dynamique du MOTD.
-if [[ -d /etc/update.motd.d ]]; then
-	find /etc/update.motd.d -mindepth 1 -maxdepth 1 -type f -delete
+if [[ -d /etc/update-motd.d ]]; then
+	find /etc/update-motd.d -mindepth 1 -maxdepth 1 -type f -delete
 fi
 
 echo "Le fichier /etc/motd a été vidé."
-echo "Les fichiers de /etc/update.motd.d ont été supprimés."
+echo "Les fichiers de /etc/update-motd.d ont été supprimés."
